db4413e9b4bdec9d5d7f6c8030de5c66d89e92c1
DrSzuriad <43809250+DrSzuriad@users.noreply.github.com>
2025-04-04 15:29:01 +0200

    Bug fix source

--- split.py
+++ split.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 
-def split_csv(input_file, output_file_1, output_file_2):
+def split_csv(input_file, output_file):
     # Wczytanie pliku CSV
-    df = pd.read_csv(input_file, encoding="utf-8")
+    df = pd.read_csv(input_file, encoding="utf-8", header=None)
     
     # Poprawa nagłówków, jeśli pierwsza kolumna zawiera błędny nagłówek
     df.columns = ["KEY", "COLUMN_2", "COLUMN_3"]
@@ -12,11 +12,10 @@ def split_csv(input_file, output_file_1, output_file_2):
     os.makedirs('en', exist_ok=True)
     
     df_1 = df[["KEY", "COLUMN_2"]]
-    df_1.to_csv(output_file_1, header=False, index=False, encoding="utf-8")
-    print(f"Plik 1 został zapisany jako: {output_file_1}")
+    df_1.to_csv(output_file, header=False, index=False, encoding="utf-8")
+    print(f"Plik 1 został zapisany jako: {output_file}")
 
 # Przykładowe użycie
-input_path = "game/Pl.csv"
-output_path_1 = "files/en/Pl.csv"
-output_path_2 = "files/pl/Pl.csv"
-split_csv(input_path, output_path_1, output_path_2)
+input_file = "game/Pl.csv"
+output_file = "files/en/Pl.csv"
+split_csv(input_file, output_file)
