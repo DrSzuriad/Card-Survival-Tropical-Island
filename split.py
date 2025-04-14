0b9149d778b71c90403c91fe5f5a949d42cbc487
DrSzuriad <43809250+DrSzuriad@users.noreply.github.com>
2025-04-14 23:43:23 +0200

    Bug fix

--- split.py
+++ split.py
@@ -3,7 +3,7 @@ import os
 
 def split_csv(input_file, output_file):
     # Wczytanie pliku CSV
-    df = pd.read_csv(input_file, encoding="utf-8", header=None)
+    df = pd.read_csv(input_file, encoding="utf-8-sig", header=None)
     
     # Poprawa nagłówków, jeśli pierwsza kolumna zawiera błędny nagłówek
     df.columns = ["KEY", "COLUMN_2", "COLUMN_3"]
@@ -11,6 +11,10 @@ def split_csv(input_file, output_file):
     df_1 = df[["KEY", "COLUMN_2"]]
     df_1.to_csv(output_file, header=False, index=False, encoding="utf-8")
     print(f"Plik 1 został zapisany jako: {output_file}")
+    
+#    df_2 = df[["KEY", "COLUMN_3"]]
+#    df_2.to_csv("files/pl/Pl.csv", header=False, index=False, encoding="utf-8")
+#    print(f"Plik 2 został zapisany jako:")
 
 # Przykładowe użycie
 input_file = "game/Pl.csv"
